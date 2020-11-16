106188ea243e68275ba678ebe8b88bc7424bc3af
Aaron Wright <aaron.wright@loopio.com>
2020-11-16 10:39:01 -0500

    Updates

--- precache-manifest.09e1e305db2ff23470aeaf324a9796d1.js
+++ precache-manifest.09e1e305db2ff23470aeaf324a9796d1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c1181d2128dde5b16a0bb99dd2fd2f3",
+    "revision": "c0cf0954ae6d0dbec69536a5ecc3ea3a",
     "url": "/daily-planner/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/daily-planner/static/js/2.4ff9abd8.chunk.js"
   },
   {
-    "revision": "a0bc8ebac58af63c3d8e",
-    "url": "/daily-planner/static/js/main.21b5fab4.chunk.js"
+    "revision": "54821b2b61fd643ed9e2",
+    "url": "/daily-planner/static/js/main.f462a7b0.chunk.js"
   },
   {
     "revision": "f6ffd485c2689ab998b8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daily-planner/precache-manifest.002d47b8070fab8e9373956c36af6dbb.js"
+  "/daily-planner/precache-manifest.09e1e305db2ff23470aeaf324a9796d1.js"
 );
 
 self.addEventListener('message', (event) => {
