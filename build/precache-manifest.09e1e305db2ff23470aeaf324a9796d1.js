4fc7e78d7734b02bb8783dbeeacc98da57271c6e
Aaron Wright <aaron.wright@loopio.com>
2020-11-16 10:39:05 -0500

    Deployed to gh-pages

---
+++ build/precache-manifest.09e1e305db2ff23470aeaf324a9796d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cf0954ae6d0dbec69536a5ecc3ea3a",
+    "url": "/daily-planner/index.html"
+  },
+  {
+    "revision": "8baa8d493b4d0eced69b",
+    "url": "/daily-planner/static/js/2.4ff9abd8.chunk.js"
+  },
+  {
+    "revision": "54821b2b61fd643ed9e2",
+    "url": "/daily-planner/static/js/main.f462a7b0.chunk.js"
+  },
+  {
+    "revision": "f6ffd485c2689ab998b8",
+    "url": "/daily-planner/static/js/runtime~main.62d44102.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,18 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-    '/daily-planner/precache-manifest.002d47b8070fab8e9373956c36af6dbb.js'
+  "/daily-planner/precache-manifest.09e1e305db2ff23470aeaf324a9796d1.js"
 );
 
-self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 workbox.core.clientsClaim();
@@ -35,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-    workbox.precaching.getCacheKeyForURL('/daily-planner/index.html'),
-    {
-        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-    }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daily-planner/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
