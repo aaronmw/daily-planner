33b37f4a5e3c32cc705a8c5699ae8ebcc56fc934
Aaron Wright <aaron.wright@loopio.com>
2020-11-20 09:37:53 -0500

    Deployed to gh-pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,18 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-    '/daily-planner/precache-manifest.c38692753fd78a9259aac4f40116d426.js'
+  "/daily-planner/precache-manifest.d9ed9214a47fadeb30e8a70db542d36d.js"
 );
 
-self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 workbox.core.clientsClaim();
@@ -35,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-    workbox.precaching.getCacheKeyForURL('/daily-planner/index.html'),
-    {
-        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-    }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daily-planner/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
