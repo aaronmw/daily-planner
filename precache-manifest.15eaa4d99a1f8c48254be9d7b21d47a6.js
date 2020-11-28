7ac950f9d469eccf2bf98850f9bedfc9c5daac15
Aaron Wright <aaron.wright@loopio.com>
2020-11-28 15:22:52 -0500

    Updates

---
+++ precache-manifest.15eaa4d99a1f8c48254be9d7b21d47a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda69d983a6a795c643c0896f3a09747",
+    "url": "/daily-planner/index.html"
+  },
+  {
+    "revision": "d007440cb7af61ef27c5",
+    "url": "/daily-planner/static/js/2.c4b394f6.chunk.js"
+  },
+  {
+    "revision": "6689f125f3a3c14d10b8",
+    "url": "/daily-planner/static/js/main.9c43a295.chunk.js"
+  },
+  {
+    "revision": "f6ffd485c2689ab998b8",
+    "url": "/daily-planner/static/js/runtime~main.62d44102.js"
+  }
+]);
