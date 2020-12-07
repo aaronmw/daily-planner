d155cb83a7fef104303cddf10431732ab70c9747
Aaron Wright <aaron.wright@loopio.com>
2020-12-07 13:53:19 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daily-planner/precache-manifest.6fa766802ff82d5390477fd4c0780637.js"
+  "/daily-planner/precache-manifest.a7a893745ae172bcda8d837698d9b638.js"
 );
 
 self.addEventListener('message', (event) => {
